ce6a2a6d09e791b535e6a7b8b22915f54ce9de6e
Zin Myint Naung <zinmyintnaung@gmail.com>
2018-05-18 15:43:41 +0800

    readme updated

---
+++ BeautifulSoup/README.md
@@ -0,0 +1,2 @@
+<h3>Using BeautifulSoup 4</h3>
+<p>Refer documentation at <a href="https://www.crummy.com/software/BeautifulSoup/bs4/doc/">BeautifulSoup</a><p>
--- BeautifulSoup/scrape.py
+++ BeautifulSoup/scrape.py
@@ -5,7 +5,7 @@ html = BeautifulSoup(requests.get(url).text, "html.parser")
 
 
 items = html.find_all('div', {'class': 'book w3-row'})
-more_items = html.find_all('div', {'class': 'book-control w3-col l3 m3 s-margin-top-sm'})
+#items = html.find_all('div', {'class': 'book-control w3-col l3 m3 s-margin-top-sm'})
 
 #print(html)
 #print(items)
@@ -14,9 +14,5 @@ more_items = html.find_all('div', {'class': 'book-control w3-col l3 m3 s-margin-
 for item in items:
     print('')
     print("Name: ", item.find('div', {'class': "book-title"}).text)
-    #print("Price: ", item.find('span', {'id': "ctl00_ContentPlaceHolder1_rptBooks_ctl00_lblPrice"}).text)
+    #print("PriBlk: ", item.find_all('div', {'class': "price-after-disc"}))
     #print("", item.find('p', {'class': "skuthumb"}).text)
-
-for more_item in more_items:
-    print('')
-    print("Price: ", item.find('span', {'id': "ctl00_ContentPlaceHolder1_rptBooks_ctl04_lblPrice"}).text)
