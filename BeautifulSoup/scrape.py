81439fd27de6256d1f73e2f0faf9d12903dcb501
zinmyintnaung <zinmyintnaung@gmail.com>
2018-05-21 14:11:31 +0800

    seek site scrapping

--- BeautifulSoup/scrape.py
+++ BeautifulSoup/scrape.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup 
-url = 'https://opentrolley.com.sg/subcategory/antiques-collectibles/toys';
+url = 'https://www.seek.com.au/PHP-jobs/in-All-Melbourne-VIC'
 html = BeautifulSoup(requests.get(url).text, "html.parser")
 
-
-items = html.find_all('div', {'class': 'book w3-row'})
+foundCount = html.find('strong', {'data-automation': 'totalJobsCount'})
 #items = html.find_all('div', {'class': 'book-control w3-col l3 m3 s-margin-top-sm'})
-
-#print(html)
 #print(items)
+print(foundCount.text)
 #print(len(items), " items found")
 
-for item in items:
-    print('')
-    print("Name: ", item.find('div', {'class': "book-title"}).text)
+#for item in items:
+    #print('')
+    #print(item)
+    #print("Count: ", item.find('strong', {'class': 'lwHBT6d'}))
     #print("PriBlk: ", item.find_all('div', {'class': "price-after-disc"}))
     #print("", item.find('p', {'class': "skuthumb"}).text)
