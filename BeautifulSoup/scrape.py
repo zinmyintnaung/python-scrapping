2ce8362abf01c08a1696417275fb72d4f25a6a92
zinmyintnaung <zinmyintnaung@gmail.com>
2018-05-21 17:17:51 +0800

    more than one query

--- BeautifulSoup/scrape.py
+++ BeautifulSoup/scrape.py
@@ -1,17 +1,12 @@
 import requests
 from bs4 import BeautifulSoup 
-url = 'https://www.seek.com.au/PHP-jobs/in-All-Melbourne-VIC'
-html = BeautifulSoup(requests.get(url).text, "html.parser")
 
-foundCount = html.find('strong', {'data-automation': 'totalJobsCount'})
-#items = html.find_all('div', {'class': 'book-control w3-col l3 m3 s-margin-top-sm'})
-#print(items)
-print(foundCount.text)
-#print(len(items), " items found")
+keywordArray = ['PHP','PMP', 'ITIL']
+arr = []
+for keyword in keywordArray:
+    url = 'https://www.seek.com.au/'+ keyword +'-jobs/in-All-Melbourne-VIC'
+    html = BeautifulSoup(requests.get(url).text, "html.parser")
+    foundCount = html.find('strong', {'data-automation': 'totalJobsCount'})
+    arr.append(foundCount.text)
 
-#for item in items:
-    #print('')
-    #print(item)
-    #print("Count: ", item.find('strong', {'class': 'lwHBT6d'}))
-    #print("PriBlk: ", item.find_all('div', {'class': "price-after-disc"}))
-    #print("", item.find('p', {'class': "skuthumb"}).text)
+print(arr)
